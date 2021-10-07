eb939f1463c7ed802c650e5dea74be28e6a76978
Lukheri <luke.contrivida@gmail.com>
2021-10-07 20:52:29 +0800

    Added ghost
    +ghost enemy
    +fixed shotgun collision

--- enemy.py
+++ enemy.py
@@ -3,31 +3,22 @@ from random import randint, choice
 from support import import_folder
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, surface, image_path, animation_dict):
+    def __init__(self, surface, image_path, animation_dict, size):
         super().__init__()
-        self.import_images(image_path, animation_dict)
+        self.import_images(image_path, animation_dict, size)
         self.display_surface = surface
         self.frame_index = 0
         self.animation_speed = 0.4
         self.direction_x = 4
         self.direction_y = choice([-2, 2, 0, 0])
     
-    def import_images(self, image_path, animation_dict):
+    def import_images(self, image_path, animation_dict, size):
         self.animations = animation_dict
 
         for animation in self.animations.keys():
             full_path = image_path + animation
-            self.animations[animation] = import_folder(full_path, True, (64, 64))
-
-# "Graphics/BlueBird/", {"Flying":[], "Hit":[]}
-class Bird(Enemy):
-    def __init__(self, surface, image_path, animation_dict):
-        super().__init__(surface, image_path, animation_dict)
-        self.status = "Flying"
-
-        self.image = self.animations[self.status][self.frame_index]
-        self.rect = self.image.get_rect(center = (randint(1400, 1600), randint(64, 560)))
-
+            self.animations[animation] = import_folder(full_path, True, size)
+    
     def animate(self):
         self.frame_index += self.animation_speed
         if self.frame_index >= len(self.animations[self.status]):
@@ -42,21 +33,56 @@ class Bird(Enemy):
         self.direction_x = -2
         self.direction_y = 0
         self.status = "Hit"
+
+class Bird(Enemy):
+    def __init__(self, surface, image_path, animation_dict, size):
+        super().__init__(surface, image_path, animation_dict, size)
+        self.direction_x = 3
+        self.direction_y = choice([-3, 3, 0, 0])
+
+        self.status = "Flying"
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center = (randint(1400, 1600), randint(64, 560)))
     
     def move(self):
         self.rect.x -= self.direction_x
         self.rect.y += self.direction_y
-        # if self.rect.y <= 50:
-        #     self.direction_y = 1
-        
-        # if self.rect.y >= 400:
-        #     self.direction_y = -1
 
         if self.rect.y <= 32 or self.rect.y >= 600:
             self.direction_y *= -1
-
     
     def update(self):
         self.animate()
         self.move()
+
+class Ghost(Enemy):
+    def __init__(self, surface, image_path, animation_dict, size):
+        super().__init__(surface, image_path, animation_dict, size)
+        self.status = "Ghosting"
+
+        self.image = self.animations[self.status][self.frame_index]
+        self.rect = self.image.get_rect(center = (randint(1400, 1600), randint(64, 560)))
+
+        self.ghost_cd = 50
+        self.ghost_timer = 0
     
+    def move(self):
+        self.rect.x -= self.direction_x
+        self.rect.y += self.direction_y
+
+        if self.rect.y <= 32 or self.rect.y >= 600:
+            self.direction_y *= -1
+
+    def going_ghost(self):
+        self.ghost_timer += 1
+        if self.ghost_timer >= self.ghost_cd:
+            self.ghost_timer = 0
+            self.status = "Ghosting"
+        pass
+
+    def update(self):
+        print(self.animations["Ghosting"])
+        self.animate()
+        self.move()
+        # self.going_ghost()
--- upgrady_yourself.py
+++ upgrady_yourself.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from gun import Gun
+from random import choice
 from player import Player
 from enemy import Bird
 from settings import *
@@ -8,33 +8,49 @@ from settings import *
 class Game:
     def __init__(self, surface):
         self.display_surface = surface
+
         self.player = pygame.sprite.GroupSingle()
         self.setup_player()
 
         self.enemies = pygame.sprite.Group()
         self.enemy_respawn_rate = 50
         self.enemy_respawn_current = 0
+        self.bird = "Graphics/BlueBird/", {"Flying":[], "Hit":[]}
+        self.ghost = "Graphics/Ghost/", {"Flying":[], "Hit":[], "Ghosting":[]}
+
         self.clicking = False
 
         self.score = 0
         self.font = pygame.font.Font("freesansbold.ttf", 256)
 
-    def spawn_enemy(self):
-        if self.enemy_respawn_current >= self.enemy_respawn_rate:
-            self.enemy_respawn_current = 0
-            self.enemies.add(Bird(self.display_surface, "Graphics/BlueBird/", {"Flying":[], "Hit":[]}))
-        else:
-            self.enemy_respawn_current += 1
-    
     def setup_player(self):
         player_sprite = Player(self.display_surface)
-        self.player.add(player_sprite)
-    
-    def check_collision(self):
+        self.player.add(player_sprite)    
+
+    def spawn_enemy(self):
+        if self.score > 10:
+            enemies = choice([self.bird, self.ghost])
+            
+        else:
+            enemies = self.bird
+
+        if enemies == self.bird:
+            size = (64, 64)
+        else:
+            size = (88, 60)
+
+        if self.enemy_respawn_current >= self.enemy_respawn_rate:
+            self.enemy_respawn_current = 0
+            self.enemies.add(Bird(self.display_surface, *enemies, size))
+        else:
+            self.enemy_respawn_current += 1
+
+    def pistol_collision(self):
         player = self.player.sprite
         for enemy in self.enemies.sprites():
             if enemy.rect.colliderect(player.rect) and pygame.mouse.get_pressed()[0] and enemy.status == "Flying":
                 if not self.clicking:
+                    print(enemy.status)
                     enemy.die()
                     self.score += 1
                     self.clicking = True
@@ -42,12 +58,31 @@ class Game:
         if not player.clicking:
             self.clicking = False
     
+    def shotgun_collision(self):
+        bullets = self.player.sprite.bullet_upgrade
+
+        for bullet in bullets.sprites():
+            for enemy in self.enemies.sprites():
+                if enemy.rect.colliderect(bullet.rect) and enemy.status == "Flying":
+                    enemy.die()
+                    self.score += 1
+
     def player_miss(self):
+        if self.score > 10:
+            enemies = choice([self.bird, self.ghost])
+        else:
+            enemies = self.bird
+        
+        if enemies == self.bird:
+            size = (64, 64)
+        else:
+            size = (88, 60)
+
         player = self.player.sprite
         for enemy in self.enemies.sprites():
             if not enemy.rect.colliderect(player.rect) and pygame.mouse.get_pressed()[0]:
                 if not self.clicking:
-                    self.enemies.add(Bird(self.display_surface, "Graphics/BlueBird/", {"Flying":[], "Hit":[]}))
+                    self.enemies.add(Bird(self.display_surface, *enemies, size))
                     self.clicking = True
         
         if not player.clicking:
@@ -66,6 +101,8 @@ class Game:
             player.gun_mode = "bullet3"
 
     def run(self):
+        gun = self.player.sprite.gun.sprite
+        
         self.show_score()
 
         self.enemies.draw(self.display_surface)
@@ -75,7 +112,12 @@ class Game:
         self.player.draw(self.display_surface)
         self.player.update()
 
-        self.check_collision()
+        if gun.gun_type == "Pistol":
+            self.pistol_collision()
+        
+        if gun.gun_type == "Shotgun":
+            self.shotgun_collision()
+
         self.player_miss()
         self.change_gun()
 
